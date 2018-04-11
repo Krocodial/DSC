cad65eabd298ecddea16d7c7f5f600f5c736f9d0
krocodial <xxkraaklxx@gmail.com>
2018-04-11 11:23:33 -0700

    db access

--- dsc/settings.py
+++ dsc/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'dsc.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+	'NAME': os.getenv('MYSQL_DATABASE'),
+	'USER': os.getenv('MYSQL_USER'),
+	'PASSWORD': os.getenv('MYSQL_PASS'),
+	'HOST': os.getenv('MYSQL_HOST'),
+	'PORT': os.getenv('MYSQL_PORT')
     }
 }
 
